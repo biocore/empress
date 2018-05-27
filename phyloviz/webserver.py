f39cce000a8c01d2ec6ba0a2c7a51c58e9af7ca8
YimengYang <yiy170@ucsd.edu>
2018-05-26 17:12:16 -0700

    configured command line more

--- phyloviz/webserver.py
+++ phyloviz/webserver.py
@@ -30,12 +30,13 @@ class Application(tornado.web.Application):
         tornado.web.Application.__init__(self, handlers, **settings)
 
 
+
 @click.command()
-@click.option('--tree_file', help='The file that contains the tree')
-@click.option('--tree_format', help='The format of the tree file')
-@click.option('--internal_metadata',
+@click.option('--tree_file', '-f', required=True, help='The file that contains the tree')
+@click.option('--tree_format', '-e', help='The format of the tree file', default='newick')
+@click.option('--internal_metadata', '-i', required=True,
               help='The file that contains internal node metadata')
-@click.option('--leaf_metadata',
+@click.option('--leaf_metadata', '-l', required=True,
               help='The file that contains tip node metadata')
 def start(tree_file, tree_format, internal_metadata, leaf_metadata):
 
