4113636b9737d82dbcd0f6d93f47f3d70e755631
kcantrel <kcantrel@ucsd.edu>
2018-08-08 10:43:19 -0700

    Changed file structure to be similar to emperor

---
+++ empress/support_files/js/init_webpage.js
@@ -0,0 +1,145 @@
+"use strict";
+
+function initWebPage(edgeMetadata) {
+  $("#highlight-menu").show();
+
+  let templateMetadata = edgeMetadata[0];
+  let x = $("#highlight-options")[0];
+  const exclude = ["px", "py", "x", "y", "branch_color","branch_is_visible",
+    "longest","node_color","node_is_visible","shortest","size","width","Node_id",
+    "Parent_id", "is_tip"];
+
+  //add metadata categories to drop down menu
+  for (let property in templateMetadata) {
+    if (!($.inArray(property, exclude) >= 0)) {
+      let option = document.createElement("option");
+      option.text = property;
+      option.label = property;
+      x.add(option);
+    }
+  }
+}
+
+function fillTable(color) {
+  let tableValues;
+  $.getJSON(urls.tableURL, {color : color}, function(data) {
+    tableValues = data;
+  }).done(function() {
+    let templateMetadata = tableValues[0];
+    let grid;
+
+    // add the header row to the table
+    let columns = [];
+    for (let property in templateMetadata) {
+      let col = {
+        id  : property,
+        name : property,
+        field : property,
+        sortable: true
+      };
+      columns.push(col);
+    }
+
+    let datarows = [];
+    for(let i = 0; i < tableValues.length; i++) {
+      let dr = {};
+      for(let property in templateMetadata) {
+        dr[property] = (tableValues[i])[property];
+      }
+      datarows.push(dr);
+    }
+    let options = {
+      enableCellNavigation: true,
+      enableColumnReorder: false,
+      topPanelHeight : 0,
+      multiColumnSort: true
+    };
+    grid = new Slick.Grid("#scrolltable", datarows, columns, options);
+
+    // taken from https://github.com/mleibman/SlickGrid/blob/gh-pages/examples/example-multi-column-sort.html
+    grid.onSort.subscribe(function (e, args) {
+      let cols = args.sortCols;
+      datarows.sort(function (dataRow1, dataRow2) {
+        let field = {};
+        let sign = {};
+        let value1 = {};
+        let value2 = {};
+        let result = {};
+        for (let i = 0, l = cols.length; i < l; i++) {
+          field = cols[i].sortCol.field;
+          sign = cols[i].sortAsc ? 1 : -1;
+          value1 = dataRow1[field];
+          value2 = dataRow2[field];
+          result = (value1 == value2 ? 0 : (value1 > value2 ? 1 : -1)) * sign;
+          if (result != 0) {
+            return result;
+          }
+        }
+        return 0;
+      });
+      grid.invalidate();
+      grid.render();
+      extractLabels(grid.getData());
+    });
+  });
+}
+
+/*
+ * Extracts the coordinates/color info from metadata and format it for webgl
+ */
+function extractInfo(metaData, fields) {
+  let extractedFields = metaData.map(function(edge) {
+    let extracted = [];
+    for (let prop of fields) {
+      extracted.push(edge[prop]);
+    };
+    return extracted;
+  });
+  extractedFields = [].concat.apply([], extractedFields);
+
+  extractedFields = extractedFields.map(function(element) {
+      return (typeof element === "string" ? element.match(/.{1,2}/g) : element)
+  });
+  extractedFields = [].concat.apply([], extractedFields);
+
+  extractedFields = extractedFields.map(function(element) {
+    return (typeof element === "string" ? parseInt("0x" + element) / 255 : element)
+  });
+
+  return extractedFields;
+}
+
+// TODO: finish this method
+function createCircles(nodes) {
+  const xCenter = 0;
+  const yCenter = 1;
+  const RED = 2;
+  const GREEN = 3;
+  const BLUE = 4;
+  let circles = [];
+  let rad = 0;
+  for (let i = 0; i < nodes.lenth / 5; i = i + 5) {
+    while(rad < Math.PI) {
+      circles.push(Math.cos(rad, 1) + nodes[i + xCenter]);
+      circles.push(Math.sin(rad, 1) + nodes[i + yCenter]);
+      circles.push(nodes[i + RED]);
+      circles.push(nodes[i + GREEN]);
+      circles.push(nodes[i + BLUE]);
+      rad += Math.PI / 10;
+    }
+  }
+  return circles
+}
+
+/*
+ *Find length along the x or y axis of the tree so webgl can fit tree into a 1x1 square
+ */
+function normalizeTree(edgeMeta) {
+  const xCoords = edgeMeta.map(edge => edge.x);
+  const yCoords = edgeMeta.map(edge => edge.y);
+  const [maxX,minX] = [Math.max(...xCoords), Math.min(...xCoords)];
+  const [maxY,minY] = [Math.max(...yCoords), Math.min(...yCoords)];
+  const [xDim, yDim] = [Math.abs(maxX - minX), Math.abs(maxY - minY)];
+
+  return Math.max(xDim, yDim)
+}
---
+++ empress/tests/test_model.py
@@ -0,0 +1,244 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2018--, empress development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# ----------------------------------------------------------------------------
+import unittest
+import numpy as np
+import pandas as pd
+import io
+from pandas.util.testing import assert_frame_equal
+from empress.model import Model
+
+
+class TestModel(unittest.TestCase):
+
+    def setUp(self):
+        self.RT_X = 750
+        self.RT_Y = 450
+
+        self.tree_file = io.StringIO(r'((a, b)le1, (c,d)ri1)r;')
+
+        self.internal_md = pd.DataFrame(
+            {
+             'clade': ['left', 'right'],
+             'time': [11, 12]
+            },
+            index=['le1', 'ri1']
+        )
+
+        self.leaf_md = pd.DataFrame(
+            {
+             'species': ['a1', 'b1', 'c1', 'd1'],
+             'gram-positive': [True, False, True, False]
+            },
+            index=['a', 'b', 'c', 'd']
+        )
+
+        f_internal = io.StringIO()
+        self.internal_md.to_csv(f_internal, sep='\t')
+        f_leaf = io.StringIO()
+        self.leaf_md.to_csv(f_leaf, sep='\t')
+
+        # need to append three blank in order for file to be read
+        self.f_internal = io.StringIO('\n\n\n' + f_internal.getvalue())
+        self.f_leaf = io.StringIO(f_leaf.getvalue())
+
+        self.tree = Model(
+            self.tree_file,
+            'newick',
+            self.f_internal,
+            self.f_leaf)
+
+    def test_merge_metadata(self):
+        merge_exp = pd.DataFrame(
+            {
+                0: [np.nan, np.nan, True, 'a1'],
+                1: [np.nan, np.nan, False, 'b1'],
+                2: [np.nan, np.nan, True, 'c1'],
+                3: [np.nan, np.nan, False, 'd1'],
+                4: ['left', 11, np.nan, np.nan],
+                5: ['right', 12, np.nan, np.nan],
+            },
+            index=['clade', 'time', 'gram-positive', 'species']).T
+
+        # hack to make time column's dtype to be float64 instead of object
+        merge_exp[['time']] = merge_exp[['time']].apply(pd.to_numeric)
+
+        merge_res = self.tree.edge_metadata[[
+            'clade', 'time', 'gram-positive', 'species']].copy()
+        assert_frame_equal(merge_exp, merge_res)
+
+    def test_center(self):
+        center_exp = pd.DataFrame({
+            0: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (154.873 - self.RT_Y)],
+            1: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (745.127 - self.RT_Y)],
+            2: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (745.127 - self.RT_Y)],
+            3: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (154.873 - self.RT_Y)],
+            4: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (332.627 - self.RT_X), (self.RT_Y - self.RT_Y)],
+            5: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (1167.37 - self.RT_X), (self.RT_Y - self.RT_Y)]},
+            index=['px', 'py', 'x', 'y']).T
+        self.tree.center_tree()
+        center_res = self.tree.edge_metadata[['px', 'py', 'x', 'y']].copy()
+
+        # convert to integer to remove precision error
+        center_exp[['px', 'py', 'x', 'y']] = center_exp[[
+            'px', 'py', 'x', 'y']].astype(int)
+        center_res[['px', 'py', 'x', 'y']] = center_res[[
+            'px', 'py', 'x', 'y']].astype(int)
+        assert_frame_equal(center_exp, center_res)
+
+    def test_select_category(self):
+        self.tree.center_tree()
+        self.tree.edge_metadata['node_is_visible'] = [
+            True, True, False, False, True, True]
+        select_exp = pd.DataFrame({
+            0: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (154.873 - self.RT_Y)],
+            1: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (745.127 - self.RT_Y)],
+            4: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (332.627 - self.RT_X), (self.RT_Y - self.RT_Y)],
+            5: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (1167.37 - self.RT_X), (self.RT_Y - self.RT_Y)]},
+            index=['px', 'py', 'x', 'y']).T
+        select_res = self.tree.select_category(
+            ['px', 'py', 'x', 'y'], 'node_is_visible')
+        select_exp = select_exp[['px', 'py', 'x', 'y']].astype(int)
+        select_res = select_res[['px', 'py', 'x', 'y']].astype(int)
+        assert_frame_equal(select_exp, select_res)
+
+    def test_select_edge_category(self):
+        self.tree.center_tree()
+        select_exp = pd.DataFrame(
+            {
+                0: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (154.873 - self.RT_Y),
+                    'FFFFFF'],
+                1: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (745.127 - self.RT_Y),
+                    'FFFFFF'],
+                2: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (745.127 - self.RT_Y),
+                    'FFFFFF'],
+                3: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (154.873 - self.RT_Y),
+                    'FFFFFF'],
+                4: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (332.627 - self.RT_X), (self.RT_Y - self.RT_Y),
+                    'FFFFFF'],
+                5: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (1167.37 - self.RT_X), (self.RT_Y - self.RT_Y),
+                    'FFFFFF']
+            },
+            index=['px', 'py', 'x', 'y', 'branch_color']).T
+        select_res = self.tree.select_edge_category()
+        select_exp = select_exp[['px', 'py', 'x', 'y', 'branch_color']]
+        select_res = select_res[['px', 'py', 'x', 'y', 'branch_color']]
+        select_exp[['px', 'py', 'x', 'y']] = select_exp[[
+            'px', 'py', 'x', 'y']].astype(int)
+        select_res[['px', 'py', 'x', 'y']] = select_res[[
+            'px', 'py', 'x', 'y']].astype(int)
+        assert_frame_equal(select_exp, select_res)
+
+    def test_select_node_category(self):
+        self.tree.center_tree()
+        select_exp = pd.DataFrame(
+            {
+                0: [(37.5 - self.RT_X), (154.873 - self.RT_Y), 'FFFFFF', 1],
+                1: [(37.5 - self.RT_X), (745.127 - self.RT_Y), 'FFFFFF', 1],
+                2: [(1462.5 - self.RT_X), (745.127 - self.RT_Y), 'FFFFFF', 1],
+                3: [(1462.5 - self.RT_X), (154.873 - self.RT_Y), 'FFFFFF', 1],
+                4: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), 'FFFFFF', 1],
+                5: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), 'FFFFFF', 1]
+            },
+            index=['x', 'y', 'node_color', 'size']).T
+        select_res = self.tree.select_node_category()
+        select_exp = select_exp[['x', 'y', 'node_color', 'size']]
+        select_res = select_res[['x', 'y', 'node_color', 'size']]
+        select_exp[['x', 'y', 'size']] = select_exp[[
+            'x', 'y', 'size']].astype(int)
+        select_res[['x', 'y', 'size']] = select_res[[
+            'x', 'y', 'size']].astype(int)
+        assert_frame_equal(select_exp, select_res)
+
+    def test_update_edge_category(self):
+        self.tree.center_tree()
+        update_equal_exp = pd.DataFrame(
+            {
+                0: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (154.873 - self.RT_Y),
+                    '000000'],
+                1: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (745.127 - self.RT_Y),
+                    'FFFFFF'],
+                2: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (745.127 - self.RT_Y),
+                    'FFFFFF'],
+                3: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (154.873 - self.RT_Y),
+                    'FFFFFF'],
+                4: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (332.627 - self.RT_X), (self.RT_Y - self.RT_Y),
+                    'FFFFFF'],
+                5: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (1167.37 - self.RT_X), (self.RT_Y - self.RT_Y),
+                    'FFFFFF']
+            },
+            index=['px', 'py', 'x', 'y', 'branch_color']).T
+        update_equal_res = self.tree.update_edge_category(
+            'species', 'branch_color', equal='a1')
+        update_equal_exp = update_equal_exp[[
+            'px', 'py', 'x', 'y', 'branch_color']]
+        update_equal_res = update_equal_res[[
+            'px', 'py', 'x', 'y', 'branch_color']]
+        update_equal_exp[['px', 'py', 'x', 'y']] = update_equal_exp[[
+            'px', 'py', 'x', 'y']].astype(int)
+        update_equal_res[['px', 'py', 'x', 'y']] = update_equal_res[[
+            'px', 'py', 'x', 'y']].astype(int)
+        assert_frame_equal(update_equal_exp, update_equal_res)
+
+        update_less_exp = pd.DataFrame(
+            {
+                0: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (154.873 - self.RT_Y),
+                    '000000'],
+                1: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (745.127 - self.RT_Y),
+                    'FFFFFF'],
+                2: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (745.127 - self.RT_Y),
+                    'FFFFFF'],
+                3: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (154.873 - self.RT_Y),
+                    'FFFFFF'],
+                4: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (332.627 - self.RT_X), (self.RT_Y - self.RT_Y),
+                    '111111'],
+                5: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (1167.37 - self.RT_X), (self.RT_Y - self.RT_Y),
+                    '111111']
+            },
+            index=['px', 'py', 'x', 'y', 'branch_color']).T
+        update_less_res = self.tree.update_edge_category(
+            'time', 'branch_color', new_value='111111', lower=10)
+        update_less_exp = update_less_exp[[
+            'px', 'py', 'x', 'y', 'branch_color']]
+        update_less_res = update_less_res[[
+            'px', 'py', 'x', 'y', 'branch_color']]
+        update_less_exp[['px', 'py', 'x', 'y']
+                        ] = update_less_exp[['px', 'py', 'x', 'y']].astype(int)
+        update_less_res[['px', 'py', 'x', 'y']
+                        ] = update_less_res[['px', 'py', 'x', 'y']].astype(int)
+        assert_frame_equal(update_less_exp, update_less_res)
+
+        update_greater_exp = pd.DataFrame(
+            {
+                0: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (154.873 - self.RT_Y),
+                    '000000'],
+                1: [(332.627 - self.RT_X), (self.RT_Y - self.RT_Y), (37.5 - self.RT_X), (745.127 - self.RT_Y),
+                    'FFFFFF'],
+                2: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (745.127 - self.RT_Y),
+                    'FFFFFF'],
+                3: [(1167.37 - self.RT_X), (self.RT_Y - self.RT_Y), (1462.5 - self.RT_X), (154.873 - self.RT_Y),
+                    'FFFFFF'],
+                4: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (332.627 - self.RT_X), (self.RT_Y - self.RT_Y),
+                    '121212'],
+                5: [(self.RT_X - self.RT_X), (self.RT_Y - self.RT_Y), (1167.37 - self.RT_X), (self.RT_Y - self.RT_Y),
+                    '111111']
+            },
+            index=['px', 'py', 'x', 'y', 'branch_color']).T
+        update_greater_res = self.tree.update_edge_category(
+            'time', 'branch_color', new_value='121212', upper=12)
+        update_greater_exp = update_greater_exp[[
+            'px', 'py', 'x', 'y', 'branch_color']]
+        update_greater_res = update_greater_res[[
+            'px', 'py', 'x', 'y', 'branch_color']]
+        update_greater_exp[['px', 'py', 'x', 'y']] = update_greater_exp[[
+            'px', 'py', 'x', 'y']].astype(int)
+        update_greater_res[['px', 'py', 'x', 'y']] = update_greater_res[[
+            'px', 'py', 'x', 'y']].astype(int)
+        assert_frame_equal(update_greater_exp, update_greater_res)
+
+
+if __name__ == '__main__':
+    unittest.main()
