ac163ba062629798930df105a0673cb73229818e
kcantrel <kcantrel@ucsd.edu>
2018-09-24 10:52:00 -0700

    Finished tests

--- empress/tools.py
+++ empress/tools.py
@@ -1,7 +1,10 @@
 import io
 import math
 import skbio
+from scipy.spatial import distance
+from scipy.spatial import ConvexHull
 import pandas as pd
+import numpy as np
 from skbio import TreeNode
 
 
@@ -171,7 +174,7 @@ def read(file_name, file_format='newick'):
     return None
 
 
-def start_and_total_angle(a_1, a_2):
+def total_angle(a_1, a_2, small_sector=True):
     """ determines the starting angle of the sector and total theta of the sector.
     Note this is only to be used if the sector is less than pi radians
 
@@ -190,25 +193,31 @@ def start_and_total_angle(a_1, a_2):
         the angle of the sector
     """
     # detemines the angle of the sector as well as the angle to start drawing the sector
-    if (not (in_quad_1(a_1) and in_quad_4(a_2) or
-             in_quad_4(a_1) and in_quad_1(a_2))):
-        a_min, a_max = (min(a_1, a_2), max(a_1, a_2))
-        if a_max - a_min > math.pi:
-            a_min += 2 * math.pi
-            starting_angle = a_max
-            theta = a_min - a_max
+    if small_sector:
+        if (not (in_quad_1(a_1) and in_quad_4(a_2) or
+                 in_quad_4(a_1) and in_quad_1(a_2))):
+            a_min, a_max = (min(a_1, a_2), max(a_1, a_2))
+            if a_max - a_min > math.pi:
+                a_min += 2 * math.pi
+                starting_angle = a_max
+                theta = a_min - a_max
+            else:
+                starting_angle = a_2 if a_1 > a_2 else a_1
+                theta = abs(a_1 - a_2)
         else:
-            starting_angle = a_2 if a_1 > a_2 else a_1
-            theta = abs(a_1 - a_2)
+            starting_angle = a_1 if a_1 > a_2 else a_2
+            ending_angle = a_1 if starting_angle == a_2 else a_2
+            theta = ending_angle + abs(starting_angle - 2 * math.pi)
     else:
-        starting_angle = a_1 if a_1 > a_2 else a_2
-        ending_angle = a_1 if starting_angle == a_2 else a_2
-        theta = ending_angle + abs(starting_angle - 2 * math.pi)
+        theta = 2 * math.pi - abs(a_1 - a_2)
 
-    return (starting_angle, theta)
+    return theta
 
 
 def extract_color(color):
+    """
+    A 6 digit hex string representing an (r, g, b) color
+    """
     HEX_BASE = 16
     NUM_CHAR = 2
     LARGEST_COLOR = 255
@@ -220,13 +229,16 @@ def extract_color(color):
 
 
 def create_arc_sector(sector_info):
+    """
+    Creates an arc using sector_info:
+    """
     sector = []
     theta = sector_info['theta'] / NUM_TRI
     rad = sector_info['starting_angle']
     (red, green, blue) = extract_color(sector_info['color'])
     c_x = sector_info['center_x']
     c_y = sector_info['center_y']
-    arc_length = sector_info['arc_length']
+    longest_branch = sector_info['largest_branch']
 
     #  creating the sector
     for i in range(0, NUM_TRI):
@@ -238,8 +250,8 @@ def create_arc_sector(sector_info):
         sector.append(blue)
 
         # second vertice of triangle
-        sector.append(math.cos(rad) * arc_length + c_x)
-        sector.append(math.sin(rad) * arc_length + c_y)
+        sector.append(math.cos(rad) * longest_branch + c_x)
+        sector.append(math.sin(rad) * longest_branch + c_y)
         sector.append(red)
         sector.append(green)
         sector.append(blue)
@@ -247,10 +259,100 @@ def create_arc_sector(sector_info):
         rad += theta
 
         # third vertice of triangle
-        sector.append(math.cos(rad) * arc_length + c_x)
-        sector.append(math.sin(rad) * arc_length + c_y)
+        sector.append(math.cos(rad) * longest_branch + c_x)
+        sector.append(math.sin(rad) * longest_branch + c_y)
         sector.append(red)
         sector.append(green)
         sector.append(blue)
 
     return sector
+
+
+def is_sector_large(points):
+    """
+    determines if a sector spans more than pi / 2 radians
+
+    parameters
+    ----------
+    points : np.array
+        the set of points that define the sector
+
+    Return
+    ------
+    boolean
+        True if sector is larger than pi / 2 radians False otherwise
+    """
+    # find angle of sector by find the two points that are adjacent to (clade.x2, clade.y2)
+    # in the convex hull
+    center = 0
+    hull = ConvexHull(points)
+    hull_vertices = hull.vertices
+    clade_index = -1
+
+    for i in range(0, len(hull_vertices)):
+        if hull_vertices[i] == center:
+            clade_index = i
+    return False if clade_index == -1 else True
+
+def sector_info(points, center, ancestor_coords):
+    """
+    'create_sector' will find the left most branch, right most branch, deepest branch, and
+    shortes branch of the clade. Then, 'create_sector' will also find the angle between the
+    left and right most branch.
+    Parameter
+    ---------
+    points : 2-d list
+        format of list [[x1,y1, x2, y2],...]
+    center : list
+        the point in points that will be used as the center of the sector. Note center
+        should not be in points
+    ancestor_coords : list
+        the coordinates of the direct parent of center. Note ancestor_coords should not
+        be in points
+    Return
+    ------
+    sector_info : Dictionary
+        The keys are center_x, center_y, starting_angle, theta, largest_branch, smallest_branch
+    """
+    # origin
+    center_point = np.array([[0, 0]])
+    center = (0, 0)
+
+    # find the length of the smallest and longest branches
+    distances = [distance.euclidean(tip, center) for tip in points]
+    longest_branch = max(distances)
+    smallest_branch = min(distances)
+
+    # calculate angles of the tip vectors
+    angles = [calculate_angle(points[x]) for x in range(0, len(points))]
+    angles = sorted(angles)
+
+    # calculate the angle of the vector going from clade root to its direct ancestor
+    ancestor_angle = calculate_angle((ancestor_coords))
+
+    # find position of the left most branch
+    num_angles = len(angles)
+    l_branch = [i for i in range(0, num_angles - 1) if angles[i] < ancestor_angle < angles[i + 1]]
+    l_found = len(l_branch) > 0
+    l_index = l_branch[0] if l_found else 0
+
+    # the left and right most branches
+    (a_1, a_2) = (angles[l_index], angles[l_index + 1]) if l_found else (angles[l_index], angles[-1])
+
+    # detemines the starting angle(left most branch) of the sectorr
+    if l_found:
+        starting_angle = a_1 if a_1 > a_2 else a_2
+    else:
+        starting_angle = a_2 if a_1 > a_2 else a_1
+
+    # calculate the angle between the left and right most branches
+    large_sector = is_sector_large(np.concatenate((center_point, points), axis=0))
+    theta = total_angle(a_1, a_2, large_sector)
+
+    # the sector webgl will draw
+    colored_clades = {
+        'center_x': center[0], 'center_y': center[1],
+        'starting_angle': starting_angle, 'theta': theta,
+        'largest_branch': longest_branch, 'smallest_branch': smallest_branch}
+
+    return colored_clades
