555809981010607043909d820510e3c2c65c795f
kcantrel <kcantrel@ucsd.edu>
2019-09-18 23:25:18 -0700

    removed Travis tests

--- tests/test-hello.js
+++
@@ -1,9 +0,0 @@
-/* Hello world test for headless chrome test */
-
-require([], function() {
-    // $(document).ready(function() {
-        QUnit.test('hello world', function(assert) {
-            assert.equal(1,1,'works');
-        });
-    // });
-})
