2d66298081b987e0e0203fb81980b9f44a04e2c8
Marcus Fedarko <mfedarko@umd.edu>
2020-04-23 14:25:50 -0700

    TST: Add test-colorer.js skeleton #142 #137

---
+++ tests/test-colorer.js
@@ -0,0 +1,15 @@
+require(['jquery', 'Colorer'], function($, Colorer) {
+    $(document).ready(function() {
+        // Setup test variables
+        module('Colorer' , {
+            setup: function() {
+            },
+
+            teardown: function() {
+            }
+        });
+        test('Test nothing right now', function() {
+            expect(0);
+        });
+    });
+});
