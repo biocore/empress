7f5d4d6320c391d9faf557e076ce121a5c514b90
Marcus Fedarko <mfedarko@umd.edu>
2020-04-23 14:33:28 -0700

    TST: Test that QIIME colorscheme matches Emperor
    :100:

--- tests/test-colorer.js
+++ tests/test-colorer.js
@@ -8,8 +8,20 @@ require(['jquery', 'Colorer'], function($, Colorer) {
             teardown: function() {
             }
         });
-        test('Test nothing right now', function() {
-            expect(0);
+        test('Test that default QIIME colors are correct', function() {
+            // I copied this in from https://github.com/biocore/emperor/blob/659b62a9f02a6423b6258c814d0e83dbfd05220e/emperor/support_files/js/color-view-controller.js#L624,
+            // so this lets us guarantee that (at least in terms of the default
+            // discrete color values) Emperor and Empress are consistent >:)
+            var exp_qiime_colors = [
+                '#ff0000', '#0000ff', '#f27304', '#008000', '#91278d',
+                '#ffff00', '#7cecf4', '#f49ac2', '#5da09e', '#6b440b',
+                '#808080', '#f79679', '#7da9d8', '#fcc688', '#80c99b',
+                '#a287bf', '#fff899', '#c49c6b', '#c0c0c0', '#ed008a',
+                '#00b6ff', '#a54700', '#808000', '#008080'
+            ];
+            for (var i = 0; i < exp_qiime_colors.length; i++) {
+                equal(Colorer.__qiimeDiscrete[i], exp_qiime_colors[i]);
+            }
         });
     });
 });
