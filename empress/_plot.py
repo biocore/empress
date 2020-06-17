3aab1806abf795e9106551e904a6f7e80e4bd874
Erfan Sayyari <esayyari@ucsd.edu>
2020-06-17 10:29:09 -0700

    adding filter unobserved features from phylogeny

--- empress/_plot.py
+++ empress/_plot.py
@@ -30,7 +30,8 @@ def plot(output_dir: str, tree: NewickFormat, feature_table: pd.DataFrame,
          feature_metadata: qiime2.Metadata = None,
          ignore_missing_samples: bool = False,
          filter_missing_features: bool = False,
-         number_of_features: int = 5) -> None:
+         number_of_features: int = 5,
+         filter_unobserved_features_from_phylogeny: bool = False) -> None:
     # TODO: do not ignore the feature metadata when specified by the user
     if feature_metadata is not None:
         feature_metadata = feature_metadata.to_dataframe()
@@ -55,7 +56,8 @@ def plot(output_dir: str, tree: NewickFormat, feature_table: pd.DataFrame,
                   sample_metadata=sample_metadata,
                   feature_metadata=feature_metadata, ordination=pcoa,
                   ignore_missing_samples=ignore_missing_samples,
-                  filter_missing_features=filter_missing_features)
+                  filter_missing_features=filter_missing_features,
+                  filter_unobserved_features_from_phylogeny=filter_unobserved_features_from_phylogeny)
 
     with open(os.path.join(output_dir, 'empress.html'), 'w') as file:
         file.write(str(viz))
