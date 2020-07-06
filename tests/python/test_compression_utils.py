e8e5235193f0201fd92833611ada633b74d788c7
Marcus Fedarko <mfedarko@umd.edu>
2020-07-06 14:32:14 -0700

    TST: add no-empty-feature tst

--- tests/python/test_compression_utils.py
+++ tests/python/test_compression_utils.py
@@ -119,8 +119,25 @@ class TestCompressionUtils(unittest.TestCase):
             ]
         )
 
-    def test_compress_table_empty_feature(self):
-        pass
+    def test_compress_table_no_empty_features(self):
+        # Prevent feature "e" from being empty
+        diff_table = self.table.copy()
+        diff_table.loc["e", "Sample3"] = 100
+        s_ids, f_ids, sid2idx, fid2idx, tbl = compress_table(diff_table)
+        self.assertEqual(s_ids, ["Sample1", "Sample2", "Sample3"])
+        self.assertEqual(f_ids, ["a", "b", "e", "d"])
+        self.assertEqual(
+            sid2idx, {"Sample1": 0, "Sample2": 1, "Sample3": 2}
+        )
+        self.assertEqual(fid2idx, {"a": 0, "b": 1, "e": 2, "d": 3})
+        self.assertEqual(
+            tbl,
+            [
+                [0, 1, 3],  # Sample1 contains features a, b, d
+                [0, 1, 3],  # Sample2 contains features a, b, d
+                [0, 2]      # Sample3 contains feature  e only
+            ]
+        )
 
     def test_compress_table_all_empty(self):
         pass
