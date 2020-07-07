748315fb11cdbcb2241e17c8ed880a790ecf3dcc
Marcus Fedarko <mfedarko@umd.edu>
2020-07-07 12:37:42 -0700

    DOC: add copyright header to compression_utils
    address @ElDeveloper comment

--- empress/compression_utils.py
+++ empress/compression_utils.py
@@ -1,3 +1,11 @@
+# ----------------------------------------------------------------------------
+# Copyright (c) 2016-2020, empress development team.
+#
+# Distributed under the terms of the Modified BSD License.
+#
+# The full license is in the file LICENSE, distributed with this software.
+# ----------------------------------------------------------------------------
+
 def compress_table(table):
     """Converts a feature table to a space-saving format.
 
