f885de9065d71bd79932f39a470b0aae282c4d49
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2020-07-10 16:01:18 -0700

    Update empress/support_files/js/node-click-callback.js
    Co-authored-by: Marcus Fedarko <marcus.fedarko@gmail.com>

--- empress/support_files/js/node-click-callback.js
+++ empress/support_files/js/node-click-callback.js
@@ -7,7 +7,6 @@ empress.setOnNodeMenuVisibleCallback(function (samples) {
 
 empress.setOnNodeMenuHiddenCallback(function (samples) {
     samples = ec.decModels.models.scatter.getPlottableByIDs(samples);
-
     ec.decViews.scatter.setEmissive(0x000000, samples);
     ec.sceneViews[0].needsUpdate = true;
 });
