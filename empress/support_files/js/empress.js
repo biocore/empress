e751c7edf0f699ca838b6b3319881616ad97f623
Marcus Fedarko <mfedarko@umd.edu>
2020-07-21 16:56:26 -0700

    DOC: add note to area comp re: c0/c1 stuff
    Relates to #184. Mostly just want to have this all documented so
    I don't forget it in five seconds.

--- empress/support_files/js/empress.js
+++ empress/support_files/js/empress.js
@@ -1242,9 +1242,30 @@ define([
             node = this._treeData[i];
             // NOTE: It is worth noting that getX() and getY() only consider
             // the xc1 / yc1 positions of nodes in the circular layout (not
-            // the xc0 / yc0 positions). I don't *think* this should be a
-            // problem, but it is probably worth testing that degenerate cases
-            // like 1-leaf trees are handled properly.
+            // the xc0 / yc0 positions). The c1 coordinates for a node in the
+            // circular layout are the "endpoints" of the node, while the c0
+            // coordinates are the start points (occuring closer to the root of
+            // the tree, which is centered at (0, 0). Zooming in on the top
+            // part of a circular layout, these coordinates (for two nodes, say
+            // n and n') would look something like:
+            //
+            //   c1     c1'
+            //   |       |
+            //   |       |
+            //   c0-----c0'
+            //       |
+            //       |
+            //
+            // I *think* we can always infer the exact minimum bounding box of
+            // the tree by only considering c1 coordinates, since the c0
+            // coordinates should always be closer to (0, 0) than the c1
+            // coordinates. However, it's conceivable that there are degenerate
+            // cases I'm not aware of that would break this -- or,
+            // alternatively, that changes to the circular layout in the future
+            // could break this. However, I imagine the vast majority of trees
+            // should be fine with this as is. (Even if things are messed up,
+            // the only consequence will be the bounding box here (and
+            // centerLayoutAvgPoint(), I suppose) being slightly off.)
             x = this.getX(node);
             y = this.getY(node);
             minX = Math.min(x, minX);
