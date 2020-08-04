c597c7ed519f205a247d7afe3bf49a90b9244ab7
kcantrel <kcantrel@ucsd.edu>
2020-08-04 14:47:41 -0700

    fixed style

--- empress/support_files/js/animator.js
+++ empress/support_files/js/animator.js
@@ -251,14 +251,17 @@ define(["Colorer", "util"], function (Colorer, util) {
 
         // Animation loop
         setTimeout(function loop() {
-            console.log(scope.curFrame)
-            if (!scope.pause && scope.curFrame + 1 < scope.totalFrames ) {
+            console.log(scope.curFrame);
+            if (!scope.pause && scope.curFrame + 1 < scope.totalFrames) {
                 if (!scope.framesRdy[scope.curFrame + 1]) {
                     scope._collectFrame(scope.curFrame + 1);
                 }
                 scope.nextFrame();
                 setTimeout(loop, scope.timePerFram);
-            } else if (!scope.pause && scope.curFrame + 1 === scope.totalFrames) {
+            } else if (
+                !scope.pause &&
+                scope.curFrame + 1 === scope.totalFrames
+            ) {
                 util.toastMsg("Animation Complete.");
             }
         }, 0);
