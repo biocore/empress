5c15d18f9a26d29fd94d9c89e641ac6d735c7d81
Marcus Fedarko <mfedarko@umd.edu>
2020-08-19 13:45:56 -0700

    TST: rename and expand barplot tests

---
+++ tests/test-barplots.js
@@ -0,0 +1,167 @@
+require(["jquery", "underscore", "Empress", "UtilitiesForTesting"], function (
+    $,
+    _,
+    Empress,
+    UtilitiesForTesting
+) {
+    module("Barplots", {
+        setup: function () {
+            this.testData = UtilitiesForTesting.getTestData();
+            this.initTestEmpress = function (defaultLayout = "Rectangular") {
+                return new Empress(
+                    this.testData.tree,
+                    this.testData.treeData,
+                    this.testData.tdToInd,
+                    this.testData.nameToKeys,
+                    this.testData.layoutToCoordSuffix,
+                    defaultLayout,
+                    this.testData.yrscf,
+                    this.testData.biom,
+                    this.testData.fmCols,
+                    this.testData.tm,
+                    this.testData.im,
+                    this.testData.canvas
+                );
+            };
+        },
+    });
+
+    test("Barplot panel is initialized with one layer", function () {
+        var empress = this.initTestEmpress();
+        equal(empress._barplotPanel.layers.length, 1);
+    });
+    test("Layout availability toggling: initialization", function () {
+        // The default layout should influence whether the barplot panel's
+        // "available" or "unavailable" content is shown. First, let's try the
+        // unrooted layout, which doesn't support barplots.
+        var empress = this.initTestEmpress("Unrooted");
+        ok(empress._barplotPanel.availContent.classList.contains("hidden"));
+        notOk(
+            empress._barplotPanel.unavailContent.classList.contains("hidden")
+        );
+
+        // And now let's try starting out in the rectangular layout, which
+        // *does* support barplots.
+        var empress2 = this.initTestEmpress("Rectangular");
+        notOk(empress2._barplotPanel.availContent.classList.contains("hidden"));
+        ok(empress2._barplotPanel.unavailContent.classList.contains("hidden"));
+    });
+    test("Layout availability toggling post-initialization", function () {
+        var empress = this.initTestEmpress("Unrooted");
+        // We need to call this in order to make updateLayout() work.
+        // Otherwise, things start breaking -- see
+        // https://github.com/biocore/empress/pull/320 for context.
+        empress.initialize();
+
+        // After updating the layout to something that supports barplots, the
+        // barplot "available content" should now be shown.
+        empress.updateLayout("Rectangular");
+        notOk(empress._barplotPanel.availContent.classList.contains("hidden"));
+        ok(empress._barplotPanel.unavailContent.classList.contains("hidden"));
+
+        // ... And going back to a not-compatible-with-barplots layout should
+        // switch back to the unavailable content.
+        empress.updateLayout("Unrooted");
+        ok(empress._barplotPanel.availContent.classList.contains("hidden"));
+        notOk(
+            empress._barplotPanel.unavailContent.classList.contains("hidden")
+        );
+    });
+    test("Barplot layers default to feature metadata layers, but only if feature metadata is available", function () {
+        // (Sample metadata should always be available. (... That is, until we
+        // support visualizing a tree without table / sample metadata info.)
+        var empressWithFM = this.initTestEmpress();
+        equal(empressWithFM._barplotPanel.layers[0].barplotType, "fm");
+
+        var empressWithNoFM = new Empress(
+            this.testData.tree,
+            this.testData.treeData,
+            this.testData.tdToInd,
+            this.testData.nameToKeys,
+            this.testData.layoutToCoordSuffix,
+            "Rectangular",
+            this.testData.yrscf,
+            this.testData.biom,
+            [],
+            {},
+            {},
+            this.testData.canvas
+        );
+        equal(empressWithNoFM._barplotPanel.layers[0].barplotType, "sm");
+    });
+    test("BarplotPanelHandler.addLayer", function () {
+        var scope = this;
+        var empress = this.initTestEmpress();
+
+        // Add on two layers
+        empress._barplotPanel.addLayer();
+        equal(empress._barplotPanel.layers.length, 2);
+        empress._barplotPanel.addLayer();
+        equal(empress._barplotPanel.layers.length, 3);
+
+        // Check that each layer was provided correct information
+        _.each(empress._barplotPanel.layers, function (layer, i) {
+            // Basic information about the visualization -- should be the same
+            // across every layer
+            equal(layer.fmCols, scope.testData.fmCols);
+            equal(layer.smCols, empress._barplotPanel.smCols);
+            equal(layer.barplotPanel, empress._barplotPanel);
+            equal(layer.layerContainer, empress._barplotPanel.layerContainer);
+            // Check that the "num" and "unique num" of each barplot layer were
+            // assigned correctly. Since no layers have been removed, these
+            // numbers should be identical.
+            equal(layer.num, i + 1);
+            equal(layer.uniqueNum, i + 1);
+            // Check that each layer's header says "Layer N" (N = layer.num)
+            equal(layer.headerElement.innerText, "Layer " + (i + 1));
+        });
+    });
+    test("BarplotPanelHandler.removeLayer", function () {
+        var scope = this;
+        var empress = this.initTestEmpress();
+        empress._barplotPanel.addLayer();
+        equal(empress._barplotPanel.layers.length, 2);
+        empress._barplotPanel.addLayer();
+        equal(empress._barplotPanel.layers.length, 3);
+
+        // Remove the second of the three layers
+        empress._barplotPanel.removeLayer(2);
+        // Check that now there are only two layers
+        equal(empress._barplotPanel.layers.length, 2);
+        // Check that layer 1's number stayed the same, while layer 3's number
+        // was decremented
+        equal(empress._barplotPanel.layers[0].num, 1);
+        equal(empress._barplotPanel.layers[1].num, 2);
+        // Check that layer 1's header stayed the same, while layer 3's header
+        // was decremented along with its number
+        equal(empress._barplotPanel.layers[0].headerElement.innerText, "Layer 1");
+        equal(empress._barplotPanel.layers[1].headerElement.innerText, "Layer 2");
+        // Check that the *unique numbers* of each layer remained the same
+        // (so that the HTML elements created for each layer will have distinct
+        // IDs)
+        equal(empress._barplotPanel.layers[0].uniqueNum, 1);
+        equal(empress._barplotPanel.layers[1].uniqueNum, 3);
+
+        // Try adding on a new layer. It should be named "Layer 3" (and have a
+        // number of 3), but its unique number should be 4 -- since it's the
+        // fourth layer created thus far, ignoring all removal operations.
+        empress._barplotPanel.addLayer();
+        equal(empress._barplotPanel.layers.length, 3);
+
+        // Verify all the layers' numbers / headers / unique numbers correct
+        // (Layer 1 and 2 (previously Layer 3) shouldn't have changed, but we
+        // might as well verify they don't break when we add a new layer after
+        // them.)
+        equal(empress._barplotPanel.layers[0].num, 1);
+        equal(empress._barplotPanel.layers[1].num, 2);
+        equal(empress._barplotPanel.layers[2].num, 3);
+        equal(empress._barplotPanel.layers[0].headerElement.innerText, "Layer 1");
+        equal(empress._barplotPanel.layers[1].headerElement.innerText, "Layer 2");
+        equal(empress._barplotPanel.layers[2].headerElement.innerText, "Layer 3");
+        equal(empress._barplotPanel.layers[0].uniqueNum, 1);
+        equal(empress._barplotPanel.layers[1].uniqueNum, 3);
+        // This is the most important check -- the newest layer should have a
+        // distinct unique number.
+        equal(empress._barplotPanel.layers[2].uniqueNum, 4);
+    });
+});
