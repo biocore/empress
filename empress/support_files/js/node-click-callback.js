09e4d1c4c31fad312e42ccbcba65387b1707b34b
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2020-09-14 10:43:23 -0700

    BUG: Fix issue with biplot and emissive selection
    Thanks @fedarko

--- empress/support_files/js/node-click-callback.js
+++ empress/support_files/js/node-click-callback.js
@@ -26,8 +26,18 @@ ec.sceneViews[0].on("click", function (name, object) {
 ec.controllers.color.addEventListener("value-double-clicked", function (
     payload
 ) {
+    // when dealing with a biplot ignore arrow-emitted events
+    if (payload.target.decompositionName() !== 'scatter') {
+        return;
+    }
+
     // cancel any ongoing timers
     clearTimeout(empress.timer);
+
+    // reset emissive settings for all markers since an ongoing timer may have
+    // been cancelled
+    ec.decViews.scatter.setEmissive(0x000000);
+    plotView.needsUpdate = true;
 
     // if there's any coloring setup remove it, and re-enable the update button
     sPanel.sUpdateBtn.classList.remove("hidden");
