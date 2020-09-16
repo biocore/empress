b5076faee3873aeb8feedc6f6e0c6e36cff31242
Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
2020-09-16 12:49:47 -0700

    ENH: Link emperor double-click event and add timer cancelation (#370)
    * ENH: Link emperor double-click event and add timer cancelation
    
    Fixes #197
    
    * BUG: Fix issue with biplot and emissive selection
    
    Thanks @fedarko
    
    * STY: Make it prettier

--- empress/support_files/js/node-click-callback.js
+++ empress/support_files/js/node-click-callback.js
@@ -22,3 +22,41 @@ ec.sceneViews[0].on("click", function (name, object) {
 
     empress.showNodeMenuForName(name);
 });
+
+ec.controllers.color.addEventListener("value-double-clicked", function (
+    payload
+) {
+    // when dealing with a biplot ignore arrow-emitted events
+    if (payload.target.decompositionName() !== "scatter") {
+        return;
+    }
+
+    // cancel any ongoing timers
+    clearTimeout(empress.timer);
+
+    // reset emissive settings for all markers since an ongoing timer may have
+    // been cancelled
+    ec.decViews.scatter.setEmissive(0x000000);
+    plotView.needsUpdate = true;
+
+    // if there's any coloring setup remove it, and re-enable the update button
+    sPanel.sUpdateBtn.classList.remove("hidden");
+    sPanel.fUpdateBtn.classList.remove("hidden");
+    legend.clear();
+    empress.resetTree();
+
+    var names = _.map(payload.message.group, function (item) {
+        return item.name;
+    });
+    var container = {};
+    container[payload.message.attribute] = names;
+    empress.colorSampleGroups(container);
+
+    // 4 seconds before resetting
+    empress.timer = setTimeout(function () {
+        empress.resetTree();
+        empress.drawTree();
+
+        plotView.needsUpdate = true;
+    }, 4000);
+});
