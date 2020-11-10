3c9610512a67b0fd06ede9c19ecb0c63a9360cec
gwarmstrong <garmstrong@colgate.edu>
2020-11-10 00:36:24 -0800

    STY command tests

--- tests/test-commands.js
+++ tests/test-commands.js
@@ -16,7 +16,7 @@ require(["jquery", "Commands"], function ($, Commands) {
                 class MockEmpress {
                     constructor() {
                         var scope = this;
-                        Object.keys(setupScope.treeDefaults).forEach(key => {
+                        Object.keys(setupScope.treeDefaults).forEach((key) => {
                             var value = setupScope.treeDefaults[key];
                             scope[key] = value;
                         });
@@ -25,15 +25,20 @@ require(["jquery", "Commands"], function ($, Commands) {
                     resetTree() {
                         var scope = this;
                         this.reset = this.reset + 1;
-                        Object.keys(setupScope.treeDefaults).forEach(key => {
-                            if (key !== 'reset') {
+                        Object.keys(setupScope.treeDefaults).forEach((key) => {
+                            if (key !== "reset") {
                                 var value = setupScope.treeDefaults[key];
                                 scope[key] = value;
                             }
                         });
                     }
 
-                    colorByFeatureMetadata(colorBy, colorMapName, coloringMethod, reverse = false) {
+                    colorByFeatureMetadata(
+                        colorBy,
+                        colorMapName,
+                        coloringMethod,
+                        reverse = false
+                    ) {
                         this.colorCategory = colorBy;
                         this.colorMap = colorMapName;
                         this.coloringMethod = coloringMethod;
@@ -44,7 +49,7 @@ require(["jquery", "Commands"], function ($, Commands) {
                         this.collapsed = true;
                     }
 
-                    thickenColoredNodes(lineWidth){
+                    thickenColoredNodes(lineWidth) {
                         this.lineThickness = lineWidth;
                     }
 
@@ -69,50 +74,50 @@ require(["jquery", "Commands"], function ($, Commands) {
             equal(empress.reset, 2);
         });
 
-        test("Test NullCommand", function() {
+        test("Test NullCommand", function () {
             var empress = new this.MockEmpress();
             var nullCommand = new Commands.NullCommand();
             nullCommand.execute(empress);
             // cannot use equal function on treeDefaults and empress
-            Object.keys(this.treeDefaults).forEach(key => {
+            Object.keys(this.treeDefaults).forEach((key) => {
                 var exp = this.treeDefaults[key];
                 var obs = empress[key];
                 equal(obs, exp, key);
             });
         });
 
-        test("Test ColorByFeatureMetadataCommand", function() {
+        test("Test ColorByFeatureMetadataCommand", function () {
             var empress = new this.MockEmpress();
             var colorCommand = new Commands.ColorByFeatureMetadataCommand({
-                colorBy: 'color-category',
-                colorMapName: 'gimme-color-map-name',
-                coloringMethod: 'gimme-method'
+                colorBy: "color-category",
+                colorMapName: "gimme-color-map-name",
+                coloringMethod: "gimme-method",
             });
             colorCommand.execute(empress);
 
-            equal(empress.colorCategory, 'color-category');
-            equal(empress.colorMap, 'gimme-color-map-name');
-            equal(empress.coloringMethod, 'gimme-method');
+            equal(empress.colorCategory, "color-category");
+            equal(empress.colorMap, "gimme-color-map-name");
+            equal(empress.coloringMethod, "gimme-method");
             equal(empress.reverse, false);
         });
 
-        test("Test ColorByFeatureMetadataCommand (reverse = true)", function() {
+        test("Test ColorByFeatureMetadataCommand (reverse = true)", function () {
             var empress = new this.MockEmpress();
             var colorCommand = new Commands.ColorByFeatureMetadataCommand({
-                colorBy: 'color-category',
-                colorMapName: 'gimme-color-map-name',
-                coloringMethod: 'gimme-method',
-                reverseColorMap: true
+                colorBy: "color-category",
+                colorMapName: "gimme-color-map-name",
+                coloringMethod: "gimme-method",
+                reverseColorMap: true,
             });
             colorCommand.execute(empress);
 
-            equal(empress.colorCategory, 'color-category');
-            equal(empress.colorMap, 'gimme-color-map-name');
-            equal(empress.coloringMethod, 'gimme-method');
+            equal(empress.colorCategory, "color-category");
+            equal(empress.colorMap, "gimme-color-map-name");
+            equal(empress.coloringMethod, "gimme-method");
             equal(empress.reverse, true);
         });
 
-        test("Test CollapaseCladesCommand", function() {
+        test("Test CollapaseCladesCommand", function () {
             var empress = new this.MockEmpress();
             var collapse = new Commands.CollapseCladesCommand();
             notOk(empress.collapsed);
@@ -120,17 +125,17 @@ require(["jquery", "Commands"], function ($, Commands) {
             ok(empress.collapsed);
         });
 
-        test("Test ThickenColoredNodesCommand", function() {
+        test("Test ThickenColoredNodesCommand", function () {
             var empress = new this.MockEmpress();
             var thicken = new Commands.ThickenColoredNodesCommand({
-                lineWidth: 7.24
+                lineWidth: 7.24,
             });
 
             thicken.execute(empress);
             equal(empress.lineThickness, 7.24);
         });
 
-        test("Test DrawTreCommand", function() {
+        test("Test DrawTreCommand", function () {
             var empress = new this.MockEmpress();
             var draw = new Commands.DrawTreeCommand();
             notOk(empress.drawn);
@@ -138,21 +143,21 @@ require(["jquery", "Commands"], function ($, Commands) {
             ok(empress.drawn);
         });
 
-        test("Test ColorByFeatureMetadataPipeline", function() {
+        test("Test ColorByFeatureMetadataPipeline", function () {
             var empress = new this.MockEmpress();
             var pipeline = new Commands.ColorByFeatureMetadataPipeline({
-                colorBy: 'gimme-category',
-                colorMapName: 'map-name',
-                coloringMethod: 'some-method',
+                colorBy: "gimme-category",
+                colorMapName: "map-name",
+                coloringMethod: "some-method",
                 lineWidth: 8.25,
                 reverseColorMap: true,
-                collapseClades: true
+                collapseClades: true,
             });
             pipeline.execute(empress);
             equal(empress.reset, 1);
-            equal(empress.colorCategory, 'gimme-category');
-            equal(empress.colorMap, 'map-name');
-            equal(empress.coloringMethod, 'some-method');
+            equal(empress.colorCategory, "gimme-category");
+            equal(empress.colorMap, "map-name");
+            equal(empress.coloringMethod, "some-method");
             equal(empress.lineThickness, 8.25);
             ok(empress.reverse);
             ok(empress.collapsed);
