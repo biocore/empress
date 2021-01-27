cec5ed6dadaa2df2eb49b60e18a77396cb9fcff3
gwarmstrong <garmstrong@colgate.edu>
2021-01-27 10:49:30 -0800

    ENH extract update anyDifferent

--- empress/support_files/js/layouts-util.js
+++ empress/support_files/js/layouts-util.js
@@ -373,10 +373,7 @@ define(["underscore", "VectorOps", "util"], function (_, VectorOps, util) {
             if (maxWidth < xCoord[node]) {
                 maxWidth = xCoord[node];
             }
-            anyDifferent =
-                // this expression assumes nodeLen and tree.length will both be positive
-                anyDifferent ||
-                Math.abs(tree.length(prepos) / nodeLen - 1) > TOL;
+            anyDifferent = updateAnyDifferent(anyDifferent, nodeLen, tree, prepos);
         }
         if (!anyDifferent && checkLengthsChange) {
             noLengthsChangedMsg();
@@ -619,8 +616,7 @@ define(["underscore", "VectorOps", "util"], function (_, VectorOps, util) {
 
             var nodeLen = lengthGetter(prepos);
             radius[node] = radius[parent] + nodeLen;
-            anyDifferent =
-                anyDifferent || Math.abs(nodeLen - tree.length(prepos)) > TOL;
+            anyDifferent = updateAnyDifferent(anyDifferent, nodeLen, tree, prepos);
         }
         if (!anyDifferent && checkLengthsChange) {
             noLengthsChangedMsg();
@@ -748,6 +744,23 @@ define(["underscore", "VectorOps", "util"], function (_, VectorOps, util) {
     }
 
     /**
+     *
+     * @param {Boolean} anyDifferent Indicates if any branches have different
+     *                               transformed lengths that differ from
+     *                               their original length in the tree.
+     * @param {Float} branchLen Transformed length of the branch.
+     * @param {BPTree} tree That has (potentially) been transformed.
+     * @param {Number} n Node index in tree.
+     * @returns {Boolean} Indicates if any of the branches checked in the tree
+     *                    so far are different than the transformed length.
+     */
+    function updateAnyDifferent(anyDifferent, branchLen, tree, n) {
+        anyDifferent =
+            anyDifferent || Math.abs(branchLen - tree.length(n)) > TOL;
+        return anyDifferent;
+    }
+
+    /**
      * Unrooted layout.
      *
      * REFERENCES
@@ -837,9 +850,7 @@ define(["underscore", "VectorOps", "util"], function (_, VectorOps, util) {
             minX = Math.min(minX, x2Arr[node]);
             maxY = Math.max(maxY, y2Arr[node]);
             minY = Math.min(minY, y2Arr[node]);
-
-            anyDifferent =
-                anyDifferent || Math.abs(nodeLen - tree.length(n)) > TOL;
+            anyDifferent = updateAnyDifferent(anyDifferent, nodeLen, tree, n);
         }
         if (!anyDifferent && checkLengthsChange) {
             noLengthsChangedMsg();
