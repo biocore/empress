4b8551b783c575eb60357418587186d2b1e95efa
Marcus Fedarko <mfedarko@umd.edu>
2021-02-02 20:33:47 -0800

    ENH: support -h for help in standalone CLI

--- empress/scripts/_cli.py
+++ empress/scripts/_cli.py
@@ -15,8 +15,13 @@ def empress():
     """Generates an interactive visualization of a phylogenetic tree."""
     pass
 
+# Allow using -h to show help information
+# https://click.palletsprojects.com/en/7.x/documentation/#help-parameter-customization
+CTXSETS = {"help_option_names": ["-h", "--help"]}
 
-@empress.command("tree-plot", short_help=desc.TREE_PLOT_DESC)
+@empress.command(
+    "tree-plot", short_help=desc.TREE_PLOT_DESC, context_settings=CTXSETS
+)
 @click.option("-t", "--tree", required=True, help=desc.TREE_DESC)
 @click.option("-o", "--output-dir", required=True, help=desc.OUTPUT_DIR)
 @click.option("-fm", "--feature-metadata", required=False, default=None,
@@ -41,7 +46,9 @@ def tree_plot(
     save_viz(viz, output_dir, q2=False)
 
 
-@empress.command("community-plot", short_help=desc.COMM_PLOT_DESC)
+@empress.command(
+    "community-plot", short_help=desc.COMM_PLOT_DESC, context_settings=CTXSETS
+)
 @click.option("-t", "--tree", required=True, help=desc.TREE_DESC)
 @click.option("-tbl", "--table", required=True, help=desc.TBL)
 @click.option("-sm", "--sample-metadata", required=True, help=desc.SM_DESC)
