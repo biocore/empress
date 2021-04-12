305aa470b692572e1d507f28051114a0a4dd4b93
kcantrel <kcantrel@ucsd.edu>
2021-04-12 13:50:42 -0700

    remove try-catch

--- empress/support_files/js/side-panel-handler.js
+++ empress/support_files/js/side-panel-handler.js
@@ -295,28 +295,21 @@ define(["underscore", "Colorer", "util"], function (_, Colorer, util) {
         lwInput,
         updateBtn
     ) {
-        try {
-            this.empress.resetTree();
+        this.empress.resetTree();
 
-            // hide update button
-            updateBtn.classList.add("hidden");
+        // hide update button
+        updateBtn.classList.add("hidden");
 
-            // color tree
-            this[colorMethodName]();
+        // color tree
+        this[colorMethodName]();
 
-            if (collapseChk.checked) {
-                this.empress.collapseClades();
-            }
-            var lw = util.parseAndValidateNum(lwInput);
-            this.empress.thickenColoredNodes(lw);
-
-            this.empress.drawTree();
-        } catch (err) {
-            this.empress.resetTree();
-            this.empress.drawTree();
-            this.empress.clearLegend();
-            updateBtn.classList.remove("hidden");
+        if (collapseChk.checked) {
+            this.empress.collapseClades();
         }
+        var lw = util.parseAndValidateNum(lwInput);
+        this.empress.thickenColoredNodes(lw);
+
+        this.empress.drawTree();
     };
 
     /**
