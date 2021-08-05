582c0544d5d75db2adb4328376a5b7748d1ef174
kwcantrell <kcantrel@ucsd.edu>
2021-08-05 16:15:52 -0700

    Add --no-empire flag to make-dev-page (#530)
    * added flag to make-dev-page so that only tree plot is created
    
    * addressed pr comments
    
    * fix small typo
    
    Co-authored-by: Yoshiki Vázquez Baeza <yoshiki89@gmail.com>
    Co-authored-by: Marcus Fedarko <marcus.fedarko@gmail.com>

--- tests/python/test_core.py
+++ tests/python/test_core.py
@@ -380,6 +380,8 @@ class TestCore(unittest.TestCase):
         # values, this helps with tests not breaking if any character changes
         # in # Emperor
         dict_a_cp = copy.deepcopy(DICT_A)
+        # set is_empire_plot flag to True since DICT_A sets it as False (all
+        # other test use a False value)
         dict_a_cp["is_empire_plot"] = True
         for key, value in obs.items():
             if not key.startswith('emperor_'):
